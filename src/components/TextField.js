96da80bed1b8e4b5041f1537e487dbb8d2a073cc
thogama <dethfiregrasp@gmail.com>
2022-07-03 22:31:52 -0300

    Tela de Login V1

--- src/components/TextField.js
+++ src/components/TextField.js
@@ -1,13 +1,50 @@
-import React from "react"
-import {useField} from "formik"
+import React, { useState } from "react"
+import { useField, ErrorMessage } from "formik"
+import { AiOutlineCheckCircle, AiOutlineExclamationCircle } from "react-icons/ai"
 
-export const TextField = ({label,...props})=>{
-    const [field,meta] = useField(props)
+export const TextField = ({ label, ...props }) => {
+    const [field, meta] = useField(props)
+    meta.value = props.name
+
+
+
     return (
-        <div  className="mb-2">
-            <label style={{fontWeight:600,fontSize:"15px",display:"block",marginBottom:"15px"}} {...field} {...props} htmlFor={field.name}>{label}</label>
-            
-            <input style={{minHeight:"48px"}} className="form-control shadow-none" autoComplete="off" type="text"></input>
+        <div className="mb-2">
+            <div style={{ display: "block" }}>
+
+            </div>
+            <label style={{ fontWeight: 600, fontSize: "15px", display: "block", marginBottom: "15px" }} htmlFor={field.name}>
+                {label}
+            </label>
+            <div className="myField" style={{ display: "flex", alignItems: "center" }}>
+                <input {...field} {...props} style={{ fontSize: "21px", minHeight: "48px" }}
+                    className={`form-control shadow-none ${meta.touched && meta.error && 'error ' || meta.touched && !meta.error && `success`}`}
+                    
+                    autoComplete="off" type={props.type}>
+
+
+
+                </input>
+                {meta.error && meta.touched && (
+                    <AiOutlineExclamationCircle color="red" size={20} style={{ position: "absolute", right: "15%" }} />
+                )}
+                {!meta.error && meta.touched && (
+                    <AiOutlineCheckCircle color="green" className="success" size={20} style={{border:"none" ,position: "absolute", right: "15%" }} />
+                )}
+
+
+            </div>
+
+
+
+
+
+
+
+            <ErrorMessage name={field.name} />
+
+
         </div>
     )
 }
+
--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,41 +1,55 @@
-import React from "react"
+import React, { useState } from "react"
 import Logo from "../assets2/logo-com-nome-slogan.png"
-import { Formik, Form, useField } from "formik"
+import { Formik, Form } from "formik"
 import * as Yup from "yup"
-import { TextField } from "../components/TextField"
+import { TextField } from "../components/TextField.js"
 
 
 
 function Login() {
 
-    //Singn vem de auth
-    function handleSubmit() {
-        console.log("login")
-    }
+    const [submitemail, setEmail] = useState("")
+    const [submitpassword, setPassowrd] = useState("")
+    const [lembrar, setLembrar] = useState(false)
 
     const initialValues = {
-        email: "",
-        password: "",
-        remember: false
+        email: submitemail,
+        password: submitpassword,
+        remember: lembrar
     }
 
     const mySchema = Yup.object({
         email: Yup.string()
             .email("Email Inválido")
             .required("É necessário digitar um email!"),
+
         senha: Yup.string()
-            .required("É necessário digitar uma senha!")
+            .required("É necessário digitar uma senha!") 
             .min(6, "Senha muito curta!")
             .max(10, "Senha muito longa!"),
 
     })
+
+    function handleLogin(e: any,email:string,password:string,lembrar:boolean) { 
+        e.preventDefault()
+        setEmail(email)
+        setLembrar(lembrar)
+        setPassowrd(password)
+
+        
+
+
+    }
+
+
 
 
 
 
 
     return (
-        <>
+        <> 
+            
             <div style={{ padding: "30px" }} className="login page wrapper">
                 <div style={{ display: "flex", alignItems: "center", flexDirection: "column" }} className="siginWrapper">
                     <img style={{ width: "100%" }} alt="logo" src={Logo} />
@@ -43,26 +57,53 @@ function Login() {
                     <p style={{ marginBottom: "60px", fontWeight: 700, fontSize: "16px", color: "rgb(119, 119, 119)" }}>Entre em sua conta para começar</p>
                 </div>
                 <Formik
+                    
+                    
+                    initialValues={initialValues}
+                    onSubmit={()=>{}}
                     validationSchema={mySchema}
-                    onSubmit={handleSubmit}
-                    initialValues={initialValues}>
-                    <Form>
-                        <TextField type="text" name="email" label={"Email"} />
-                        <TextField type="password" name="password" label={"Senha"} />
-                        <div style={{ display: "flex",justifyContent:"space-between" }}>
-                            <div className="form-check form-switch">
-                                <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-                                <label style={{fontWeight:600,cursor:"pointer"}} className="form-check-label" htmlFor="flexSwitchCheckDefault">Lembrar senha</label>
+                
+                    
+
+                >
+                    
+
+                    {({ errors,values }) => (
+                        
+                        <Form onSubmit={(e)=>handleLogin(e,values.email,values.password,values.remember)} >
+                                                     
+
+                            <TextField   id="email"  type="text" name="email" label={"Email"} />
+                            
+                            <TextField    type="password" name="senha" label={"Senha"} />
+                            
+                            <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", marginTop: "40px", marginBottom: "40px" }}>
+                                <div className="form-check form-switch">
+                                    <input onChange={e => setLembrar(Boolean(!lembrar))} className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+                                    <label style={{ fontWeight: 600, cursor: "pointer" }} className="form-check-label" htmlFor="flexSwitchCheckDefault">Lembrar senha</label>
+                                </div>
+                                <a href="#" style={{ textDecoration: "none", color: "rgb(0, 132, 137)", marginLeft: "10px", fontWeight: 700, fontSize: "16px" }}>Esqueceu a senha?</a>
+
                             </div>
-                            <a href="#" style={{ textDecoration: "none", color: "rgb(0, 132, 137)", marginLeft: "10px", fontWeight: 700, fontSize: "16px" }}>Esqueceu a senha?</a>
 
-                        </div>
+                            <button className="text-light hover-overlay" style={{
+                                border: "none",
+                                width: "100%", backgroundColor: "rgb(0, 132, 137)",
+                                alignItems: "center", fontWeight: 700,
+                                display: "flex", height: "57px",
+                                flexDirection: "column",
+                                justifyContent: "space-around",
+                                borderRadius: "5px"
+                            }} type="submit">
+                                Entrar
+                            </button>
 
 
 
 
-                    </Form>
 
+                        </Form>
+                    )}
 
                 </Formik>
 
@@ -72,3 +113,5 @@ function Login() {
 }
 
 export default Login
+
+
