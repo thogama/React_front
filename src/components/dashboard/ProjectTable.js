c358f299ef6ae4c3f4b6952a559526b8453e280c
thogama <dethfiregrasp@gmail.com>
2022-07-04 00:14:12 -0300

    mem limit

--- src/components/dashboard/ProjectTable.js
+++ src/components/dashboard/ProjectTable.js
@@ -1,5 +1,9 @@
 import { Card, CardBody, CardTitle, CardSubtitle, Table } from "reactstrap";
-
+import user1 from "../../assets/images/users/user1.jpg";
+import user2 from "../../assets/images/users/user2.jpg";
+import user3 from "../../assets/images/users/user3.jpg";
+import user4 from "../../assets/images/users/user4.jpg";
+import user5 from "../../assets/images/users/user5.jpg";
 
 const tableData = [
   {
