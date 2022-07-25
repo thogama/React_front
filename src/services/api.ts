fc1c1b8704742b3de2bfd6319fc7700c89ce42f7
thogama <dethfiregrasp@gmail.com>
2022-07-25 00:28:42 -0300

    create paciente com imagem

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,4 @@
 import axios from "axios"
-import { string } from "prop-types"
-import AppRoutes from "../routes/Router"
 
 
 export const api = axios.create({
@@ -11,8 +9,6 @@ export const api = axios.create({
 export const createEstabelecimento = (e: any, file: any, values: any) => {
     const data = new FormData()
     data.set("logomarca", file)
-    
-    
     data.set("email",values.email)
     data.set("senha",values.senha)
     data.set("nome",values.nome)
@@ -39,22 +35,42 @@ export const createSession = async (email: string, password: string) => {
     return api.post("/paciente/login", { email: email, senha: password })
 }
 
-export const createPaciente = async (nome: string,
-    cpf: string, plano: string, sexo: string,
-    email: string, rua: string, numero: string,
-    bairro: string, cep: string, municipio: string,
-    estado: string, hipertensao: boolean, diabetes: boolean,
-    fuma: boolean, alergia: boolean, bebida_alcoolica: boolean,
-    medicamento: string, senha: string) => {
-    return api.post("/paciente/signin", {
-        nome: nome, cpf: cpf, planoSaude: plano,
-        sexo: sexo, email: email, rua: rua, numero: numero,
-        municipio: municipio, bairro: bairro, cep: cep, estado: estado,
-        hipertensao: hipertensao, diabetes: diabetes, fuma: fuma,
-        alergia: alergia, bebidaAlcoolica: bebida_alcoolica,
-        medicamentoUso: medicamento, senha: senha
-    })
-}
+
+
+export const createPaciente = async(e:any,file:any,values:any,selects:any)=>{
+        const data = new FormData()
+        data.set("foto", file)
+
+        data.set("plano",values.plano)
+
+        data.set("sexo",selects.sexo)
+        data.set("fuma",selects.fuma)
+        data.set("hipertensao",selects.hipertensao)
+        data.set("diabetes",selects.diabetes)
+        data.set("alergia",selects.alergia)
+        data.set("bebidaAlcoolica",selects.bebida_alcoolica)
+        data.set("medicamentoUso",selects.medicamento)
+        data.set("cep",values.CEP)
+        data.set("email",values.email)
+        data.set("senha",values.senha)
+        data.set("nome",values.nome)
+
+        data.set("bairro",values.bairro) 
+        data.set("cpf",values.cpf)
+        data.set("rua",values.rua)
+        data.set("numero",values.numero)
+        data.set("municipio",values.municipio)
+        data.set("estado",values.estado)
+        console.log(data)
+        //const baseURL = "https://backend-orcilink.herokuapp.com/"
+        const baseURL = "http://localhost:3333/paciente/signin"
+    
+          axios.post(baseURL,data,).then((response)=>{
+              console.log(response)
+          })
+    
+    }
+
 
 
 
