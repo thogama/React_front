03f4e1c05f049fac8803ece032d9b38f9268b2da
thogama <dethfiregrasp@gmail.com>
2022-07-29 11:54:13 -0300

    primeiros testes

--- src/views/Agenda.tsx
+++ src/views/Agenda.tsx
@@ -5,31 +5,58 @@ import startOfWeek from "date-fns/startOfWeek";
 import React, { useState } from "react";
 import { Calendar, dateFnsLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
+import CardPesquisa from "../components/CardPesquisa";
+import pt from "date-fns/locale/pt"
+
+
 
 const locales = {
-    "pt-BR": require("date-fns/locale/pt-BR"),
-};
+  "pt-BR": pt
+}
+
+
 const localizer = dateFnsLocalizer({
-    format,
-    parse,
-    startOfWeek,
-    getDay,
-    locales,
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales
 });
 
 
 
-function App() {
-    const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
-   
+const events = [
+  {
+    title:"teste",
+    start: new Date("2022/07/27"),
+    end: new Date("2022/07/27")
 
-    return (
-        <div className="card p-4">
-            <h1>Calendar</h1>
-            {/* events={allEvents} */}
-            <Calendar localizer={localizer}   style={{ height: 500, margin: "50px" }} />
-        </div>
-    );
+  }
+]
+
+
+
+
+
+
+function App() {
+  const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
+
+
+  return (
+    <>
+      <CardPesquisa />
+      <div className="card p-4">
+        <h1>Agenda de Consultas</h1>
+        {/* events={allEvents} */}
+        <Calendar startAccessor={"start"} endAccessor={"end"}
+         events={events} culture="pt-BR" localizer={localizer} style={{ height: 500, margin: "50px" }} />
+      </div>
+
+
+    </>
+
+  );
 }
 
 export default App;
